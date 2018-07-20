8709bdf3178a596ac9e966d94d71feb947d63c35
ddayin <ddayin@hotmail.com>
2018-07-21 01:04:16 +0900

    refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,21 @@ public enum Direction
     Right
 }
 
+public enum PlayerState
+{
+    Idle = 0,
+    Move,
+    PullStick,
+    PushStick
+}
+
 public class Player : MonoBehaviour
 {
     #region Properties
     private Animator m_Animator;
     private SpriteRenderer m_Renderer;
+
+    private PlayerState m_State = PlayerState.Idle;
     private Direction m_LookDirection = Direction.Right;
     #endregion
 
@@ -21,6 +31,8 @@ public class Player : MonoBehaviour
     {
         m_Animator = this.GetComponent<Animator>();
         m_Renderer = this.GetComponent<SpriteRenderer>();
+
+        Init();
     }
 
     private void Update()
@@ -29,22 +41,67 @@ public class Player : MonoBehaviour
     }
     #endregion
 
+    private void Init()
+    {
+        m_State = PlayerState.Idle;
+        m_LookDirection = Direction.Right;
+    }
+
+    private void ChangeAnimation( PlayerState _state )
+    {
+        if ( m_State == _state ) return;
+
+        m_State = _state;
+
+        switch( m_State )
+        {
+            case PlayerState.Idle:
+            m_Animator.SetBool( "move", false );
+            break;
+
+            case PlayerState.Move:
+            m_Animator.SetBool( "move", true );
+            break;
+
+            case PlayerState.PullStick:
+            break;
+
+            case PlayerState.PushStick:
+            break;
+
+            default:
+            break;
+        }
+    }
+
     private void ProcessKey()
     {
-        m_Animator.SetBool( "move", false );
 
         if ( Input.GetKey( KeyCode.RightArrow ) == true )
         {
-            m_Animator.SetBool( "move", true );
             m_LookDirection = Direction.Right;
             m_Renderer.flipX = false;
+
+            ChangeAnimation( PlayerState.Move );
         }
+
+        if ( Input.GetKeyUp( KeyCode.RightArrow ) == true )
+        {
+            ChangeAnimation( PlayerState.Idle );
+        }
+
 
         if ( Input.GetKey( KeyCode.LeftArrow ) == true )
         {
-            m_Animator.SetBool( "move", true );
             m_LookDirection = Direction.Left;
             m_Renderer.flipX = true;
+
+            ChangeAnimation( PlayerState.Move );
+        }
+
+        if ( Input.GetKeyUp( KeyCode.LeftArrow ) == true )
+        {
+            ChangeAnimation( PlayerState.Idle );
         }
     }
 }
---
+++ Assets/Scripts/Stick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour 
+{
+	#region Properties
+	[SerializeField]
+	private float m_Height;
+
+	[SerializeField]
+	private bool m_IsGrowing = false;
+	#endregion
+
+	#region MonoBehaviour
+	void Awake()
+	{
+
+	}
+
+	void Update()
+	{
+		ProcessKeyInput();
+	}
+	#endregion
+
+	private void ProcessKeyInput()
+	{
+		if ( Input.GetKey( KeyCode.UpArrow ) == true )
+		{
+			m_IsGrowing = true;
+
+			Grow();
+		}
+
+		if ( Input.GetKeyUp( KeyCode.UpArrow ) == true )
+		{
+			m_IsGrowing = false;
+		}
+	}
+
+	private void Grow()
+	{
+		if ( m_IsGrowing == false ) return;
+	}
+
+}
