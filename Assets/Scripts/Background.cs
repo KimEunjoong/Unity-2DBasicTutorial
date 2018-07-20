b2109e86a8935124c96940fb5c122859d094b9ef
ddayin <ddayin@hotmail.com>
2018-07-20 19:41:17 +0900

    initial commit

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    #region Properties
+    /// <summary>
+    /// 
+    /// </summary>
+    private bool m_IsScrolling = false;
+
+    private Direction m_ScrollDirection = Direction.Right;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float m_ScrollSpeed = 1f;
+    #endregion
+
+    #region MonoBehaviour
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        m_IsScrolling = false;
+
+        if ( Input.GetKey( KeyCode.RightArrow ) == true )
+        {
+            m_IsScrolling = true;
+            m_ScrollDirection = Direction.Right;
+        }
+
+        if ( Input.GetKey( KeyCode.LeftArrow ) == true )
+        {
+            m_IsScrolling = true;
+            m_ScrollDirection = Direction.Left;
+        }
+
+        Scroll();
+    }
+    #endregion
+
+    /// <summary>
+    /// do scroll
+    /// </summary>
+    private void Scroll()
+    {
+        if ( m_IsScrolling == false )
+        {
+            return;
+        }
+
+        Vector3 pos = new Vector3( m_ScrollSpeed * Time.deltaTime, 0f, 0f );
+
+        if ( m_ScrollDirection == Direction.Left )
+        {
+            transform.position += pos;
+        }
+        else if ( m_ScrollDirection == Direction.Right )
+        {
+            transform.position -= pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    Left = 0,
+    Right
+}
+
+public class Player : MonoBehaviour
+{
+    #region Properties
+    private Animator m_Animator;
+    private SpriteRenderer m_Renderer;
+    private Direction m_LookDirection = Direction.Right;
+    #endregion
+
+    #region MonoBehaviour
+    private void Awake()
+    {
+        m_Animator = this.GetComponent<Animator>();
+        m_Renderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ProcessKey();
+    }
+    #endregion
+
+    private void ProcessKey()
+    {
+        m_Animator.SetBool( "move", false );
+
+        if ( Input.GetKey( KeyCode.RightArrow ) == true )
+        {
+            m_Animator.SetBool( "move", true );
+            m_LookDirection = Direction.Right;
+            m_Renderer.flipX = false;
+        }
+
+        if ( Input.GetKey( KeyCode.LeftArrow ) == true )
+        {
+            m_Animator.SetBool( "move", true );
+            m_LookDirection = Direction.Left;
+            m_Renderer.flipX = true;
+        }
+    }
+}
