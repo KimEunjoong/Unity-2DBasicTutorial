9aea73555832699d651acfb50256b0b8a72ddba5
ddayin <ddayin@hotmail.com>
2018-07-21 04:06:23 +0900

    refactor

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -6,10 +6,13 @@ public class Stick : MonoBehaviour
 {
 	#region Properties
 	[SerializeField]
-	private float m_Height;
+	private float m_Height = 0;
 
 	[SerializeField]
 	private bool m_IsGrowing = false;
+
+	[SerializeField]
+	private float m_GrowSpeed = 1f;
 	#endregion
 
 	#region MonoBehaviour
@@ -42,6 +45,9 @@ public class Stick : MonoBehaviour
 	private void Grow()
 	{
 		if ( m_IsGrowing == false ) return;
+
+		m_Height += m_GrowSpeed * Time.deltaTime;
+		transform.localScale = new Vector3( 10f, m_Height, 1f );
 	}
 
 }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public bool A_IsTodoCondition = true;
+    public bool A_IsHolding = false;
+    public bool A_IsAutoDrop = false;
+    public bool A_IsException = false;
+
+    public bool B_IsTodoCondition = true;
+    public bool B_IsHolding = false;
+    public bool B_IsAutoDrop = false;
+    public bool B_IsException = false;
+
+    void Start () {
+
+        JobRepeatManager.Instance.AddJob(string.Format("RepeatJob{0}", 1),
+            OnExcuteSample_A, 2.0f, 0,
+            () => { return A_IsTodoCondition; },
+            () => { return A_IsHolding; },
+            () => { return A_IsAutoDrop; },
+            () => { return A_IsException; });
+
+        JobRepeatManager.Instance.AddJob(string.Format("RepeatJob{0}", 2),
+            OnExcuteSample_B, 2.0f, 0,
+            () => { return B_IsTodoCondition; },
+            () => { return B_IsHolding; },
+            () => { return B_IsAutoDrop; },
+            () => { return B_IsException; });
+    }
+
+    private void OnExcuteSample_A()
+    {
+        Debug.LogFormat("Repeat_Index : A");
+    }
+
+    private void OnExcuteSample_B()
+    {
+        Debug.LogFormat("Repeat_Index : B");
+    }
+}
